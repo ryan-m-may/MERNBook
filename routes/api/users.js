4c3e650fe3af879740c3a4cd7a292deeece1e60c
IMayCode <imaycode@gmail.com>
2021-01-30 15:02:59 -0600

    use json web token for user creation

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 // User Schema
@@ -64,8 +66,23 @@ router.post(
       await user.save();
 
       // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-      res.send('User registered.');
+      jwt.sign(
+        payload,
+        config.get('jwtToken'),
+        // Set to 360000 for the sake of development
+        // Change to 3600 (one hour) for production
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw error;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
