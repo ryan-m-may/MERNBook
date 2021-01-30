94d7c77cc8b75ba881cc208f8d8f622400e77a41
IMayCode <imaycode@gmail.com>
2021-01-29 21:53:48 -0600

    add try to user route to check if user exists. Added gravatar to user route.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const { check, validationResult } = require('express-validator');
+
+// User Schema
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
 router.post(
   '/',
+  // Validate name email and password using express-validator
   [
     check('name', 'Name is required.').not().isEmpty(),
     check('email', 'Please include a valid email address.').isEmail(),
@@ -15,13 +20,34 @@ router.post(
       'Please enter a password with 6 or more characters.'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('User route');
+    // Destructuring - pull out name, email, password from req.body
+    const { name, email, password } = req.body;
+
+    // Gives a promise back (this is why this function is async)
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists."' }] });
+      }
+      // Get users gravatar
+
+      // Encrypt password using bcrypt
+
+      // Return jsonwebtoken
+
+      res.send('User route');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
