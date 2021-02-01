2df83c3d6adcbd2856a312f40b6176c1a76cc947
IMayCode <imaycode@gmail.com>
2021-01-31 20:34:10 -0600

    create schema for user profile

---
+++ models/Profile.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+});
