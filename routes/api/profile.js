d98359892165c551119976e10fc6658c3907f470
IMayCode <imaycode@gmail.com>
2021-02-01 12:10:03 -0600

    get user by ID now says profile not found if the profile isn't found or if the object ID is incorrect

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -131,12 +131,14 @@ router.get('/user/:user_id', async (req, res) => {
     const profile = await Profile.findOne({
       user: req.params.user_id,
     }).populate('user', ['name', 'avatar']);
-    if (!profile)
-      return res.status(400).json({ msg: 'There is no profile for this user' });
+    if (!profile) return res.status(400).json({ msg: 'Profile Not Found' });
 
     res.json(profile);
   } catch (err) {
     console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile Not Found' });
+    }
     res.status(500).send('Server Error');
   }
 });
