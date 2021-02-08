eb0b849de9d680569111c6bb0286fad6e9dc9f29
IMayCode <imaycode@gmail.com>
2021-02-08 14:19:54 -0600

    install react app, change express server to port 5000 so as not to interfere with react running on 3000

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
