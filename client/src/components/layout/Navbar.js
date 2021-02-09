c9b1cb96620987bab96c756e3cd7c0f5e1de0f80
IMayCode <imaycode@gmail.com>
2021-02-08 18:58:57 -0600

    errors

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default const Navbar = () => {
+  return (
+    <nav className='navbar bg-dark'>
+      <h1>
+        <a href='index.html'>
+          <i className='fas fa-code'></i> DevConnector
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <a href='profiles.html'>Developers</a>
+        </li>
+        <li>
+          <a href='register.html'>Register</a>
+        </li>
+        <li>
+          <a href='login.html'>Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
