3e48d6c034537a5444114764f32312bd95d7a76e
IMayCode <imaycode@gmail.com>
2021-02-13 00:35:55 -0600

    add dynamic bio

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -12,11 +12,7 @@ const ProfileAbout = ({
     {bio && (
       <Fragment>
         <h2 class='text-primary'>{name.trim().split(' ')[0]}'s Bio</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed
-          doloremque nesciunt, repellendus nostrum deleniti recusandae nobis
-          neque modi perspiciatis similique?
-        </p>
+        <p>{bio}</p>
         <div class='line'></div>
       </Fragment>
     )}
